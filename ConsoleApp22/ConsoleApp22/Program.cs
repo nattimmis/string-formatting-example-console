0ef814cb46823a39d6f9fd2ac7360d55517fe023
lionschasing <lionschasing@gmail.com>
2017-08-02 01:38:19 +0100

    string formatting console
    string formatting console

---
+++ ConsoleApp22/ConsoleApp22/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value1 = "Example";
+            string value2 = "second";
+            string value3 = "third";
+
+            Console.WriteLine("{0},{1},{2}",
+                value1,
+            value2,
+            value3);
+        }
+    }
+}
